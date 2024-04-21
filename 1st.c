92c7726e173842166dc7e58aea707eb0e2449611
Abhishek <abhishekmishra051028@gmail.com>
2024-04-21 12:45:52 +0530

    Some changes

--- 1st.c
+++ 1st.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     char str[1000];
-    fgets(str , sizeof(str),stdin);
-    printf("Hello world!\n%s",str);
+    fgets(str , sizeof(str),stdin); // taking input string from the user using fgets function
+    printf("Hello world!\n%s",str); // here we are trying to print hello world and given string from the user
     return 0;
 }
